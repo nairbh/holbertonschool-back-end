881333a3442faa999cf2446462e9746579d581ad
nairbh <houcinenair@proton.me>
2023-08-21 19:58:46 +0200

    update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,15 +4,16 @@ A script to export data in the CSV format.
 '''
 
 import csv
-import sys
 import requests
-
+import sys
 if __name__ == "__main__":
     get_emp_id = sys.argv[1]
     user_url = f'https://jsonplaceholder.typicode.com/users/{get_emp_id}'
     get_emp_data = requests.get(user_url).json()
 
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}'
+    todos_url = (
+        f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}'
+    )
     get_emp_tasks = requests.get(todos_url).json()
 
     with open("{}.csv".format(get_emp_id), 'w', newline='') as csvfile:
