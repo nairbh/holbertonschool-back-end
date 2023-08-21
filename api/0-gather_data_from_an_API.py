a82eb69d7c13d06404c1f25b7a5e0e2338e699de
nairbh <houcinenair@proton.me>
2023-08-21 18:04:26 +0200

    update

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-import requests, sys
+import requests
+import sys
 
 if __name__ == "__main__":
     get_emp_id = sys.argv[1]
-    get_emp_data = requests.get(f'https://jsonplaceholder.typicode.com/users/{get_emp_id}').json()
-    get_emp_tasks = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}').json()
+    get_emp_data = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{get_emp_id}').json()
+    get_emp_tasks = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}').json()
 
     done_tasks = [task for task in get_emp_tasks if task.get("completed")]
 
-    print(f"Employee {get_emp_data['name']} is done with tasks({len(done_tasks)}/{len(get_emp_tasks)}):")
+    print(
+        f"Employee {get_emp_data['name']} is done with tasks({len(done_tasks)}/{len(get_emp_tasks)}):")
     for task in done_tasks:
         print("\t", task["title"])
