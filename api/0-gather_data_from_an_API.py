bccc0d0b41aed8ed5ac6fc8fd5b23dc4392978da
nairbh <houcinenair@proton.me>
2023-08-21 18:19:34 +0200

    update pep style code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,14 +7,17 @@ import sys
 
 if __name__ == "__main__":
     get_emp_id = sys.argv[1]
-    get_emp_data = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{get_emp_id}').json()
-    get_emp_tasks = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}').json()
+    user_url = (f'https://jsonplaceholder.typicode.com/users/{get_emp_id}')
+    get_emp_data = requests.get(user_url).json()
+    todos_url = (
+        f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}')
+    get_emp_tasks = requests.get(todos_url).json()
 
     done_tasks = [task for task in get_emp_tasks if task.get("completed")]
 
     print(
-        f"Employee {get_emp_data['name']} is done with tasks({len(done_tasks)}/{len(get_emp_tasks)}):")
+        f"Employee {get_emp_data['name']} is done with "
+        f"tasks({len(done_tasks)}/{len(get_emp_tasks)}):"
+    )
     for task in done_tasks:
         print("\t", task["title"])
